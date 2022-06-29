d565e3f814dd25dca7b5c33de98e556c0ac07e64
Engaged007 <107148898+Engaged007@users.noreply.github.com>
2022-06-29 14:40:27 -0500

    29 06 22

--- sketch.js
+++ sketch.js
@@ -1,49 +1,61 @@
-//Criando as variáveis
+//variáveis
 var dinossauroCorrendoNaBlackFriday;
 var irmaoDoDinossauronaBlackFriday;
 var asfaltoquenaoeasfalto;
 var irmaodoasfalto;
-//Função para carregar os arquivos
+var aviaodamulhermaravilha;
+
+//arquivos
 function preload(){
-  irmaoDoDinossauronaBlackFriday = loadAnimation("trex1.png","trex3.png","trex4.png");  
+irmaoDoDinossauronaBlackFriday = loadAnimation("trex1.png","trex3.png","trex4.png");  
 irmaodoasfalto = loadImage ("ground.png");
 }
 
-//Função de configuração
+//configuração
 function setup(){
-  //Cria a tela
-  createCanvas(600,200);
-  asfaltoquenaoeasfalto = createSprite (200,180,400,20);
-  asfaltoquenaoeasfalto.addImage (irmaodoasfalto);
-  asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2
-  //Cria o sprite do dinossauro
-  dinossauroCorrendoNaBlackFriday = createSprite(50,160,20,50);
-  //Adiciona a animação ao dinossauro
-  dinossauroCorrendoNaBlackFriday.addAnimation("correndo", irmaoDoDinossauronaBlackFriday);
-  //Diminui ou aumenta a escala da variável
-  dinossauroCorrendoNaBlackFriday.scale = 0.5;
-  //Criando as bordas
-  borda = createEdgeSprites();
+
+//tela
+createCanvas(600,200);
+asfaltoquenaoeasfalto = createSprite (200,180,400,20);
+asfaltoquenaoeasfalto.addImage (irmaodoasfalto);
+asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2;
+aviaodamulhermaravilha = createSprite (50,190,400,10);
+aviaodamulhermaravilha.visible = false;
+
+//sprite do dinossauro
+dinossauroCorrendoNaBlackFriday = createSprite(50,160,20,50);
+
+//animação ao dinossauro
+dinossauroCorrendoNaBlackFriday.addAnimation("correndo", irmaoDoDinossauronaBlackFriday);
+
+//escala da variável
+dinossauroCorrendoNaBlackFriday.scale = 0.5;
+
+//bordas
+borda = createEdgeSprites();
+}
+//desenho
+function draw(){
+console.log (dinossauroCorrendoNaBlackFriday.y);
+
+//cor de fundo
+background("white");
+asfaltoquenaoeasfalto.velocityX = -2;
+if(asfaltoquenaoeasfalto.x<0) {
+asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2;
 }
 
-//Função de desenho
-function draw(){
-  //Definindo a cor de fundo
-  background("white");
-  asfaltoquenaoeasfalto.velocityX = -2;
-  if(asfaltoquenaoeasfalto.x<0) {
-    asfaltoquenaoeasfalto.x = asfaltoquenaoeasfalto.width/2;
-  }
+//tecla de espaço
+if(keyDown("space")&&dinossauroCorrendoNaBlackFriday.y>=150){
+dinossauroCorrendoNaBlackFriday.velocityY = -12;
+}
 
-  //Se apertar a tecla de espaço, o dinossauro pula
-  if(keyDown("space")){
-    dinossauroCorrendoNaBlackFriday.velocityY = -12;
-  }
-  //Sistema de gravidade
-  dinossauroCorrendoNaBlackFriday.velocityY += 1;
+//gravidade
+dinossauroCorrendoNaBlackFriday.velocityY += 1;
 
-  //Impede o dinossauro de cair da tela
-  dinossauroCorrendoNaBlackFriday.collide(asfaltoquenaoeasfalto);
-  //Desenha todos os sprites
-  drawSprites();
+//dinossauro não cai da tela
+dinossauroCorrendoNaBlackFriday.collide(aviaodamulhermaravilha);
+
+//Desenha todos os sprites
+drawSprites();
 }
