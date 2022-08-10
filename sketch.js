d519863eae545d62473a234d6cd8f6ec642d4faa
Engaged007 <107148898+Engaged007@users.noreply.github.com>
2022-08-10 14:52:04 -0500

    10 08 2022 16:51

--- sketch.js
+++ sketch.js
@@ -6,16 +6,31 @@ var irmaodoasfalto;
 var aviaodamulhermaravilha;
 var algodaodoce;
 var eupensaqueanuvemeraalgodaodoce;
-
+var jorginho1;
+var jorginho2;
+var jorginho3;
+var jorginho4;
+var jorginho5;
+var jorginho6;
+var PONTOSSSS;
 //arquivos
 function preload(){
 irmaoDoDinossauronaBlackFriday = loadAnimation("trex1.png","trex3.png","trex4.png");  
 eupensaqueanuvemeraalgodaodoce = loadImage ("cloud.png");
 irmaodoasfalto = loadImage ("ground.png");
+jorginho1 = loadImage("obstacle1.png");
+jorginho2 = loadImage("obstacle2.png");
+jorginho3 = loadImage("obstacle3.png");
+jorginho4 = loadImage("obstacle4.png");
+jorginho5 = loadImage("obstacle5.png");
+jorginho6 = loadImage("obstacle6.png");
 }
 
 //configuração
 function setup(){
+
+//valor dos pontos
+PONTOSSSS = 0;
 
 //tela
 createCanvas(600,200);
@@ -46,6 +61,7 @@ console.log(aleatorio);
 function draw(){
 //console.log (dinossauroCorrendoNaBlackFriday.y);
 
+
 //cor de fundo
 background("white");
 asfaltoquenaoeasfalto.velocityX = -2;
@@ -66,9 +82,14 @@ dinossauroCorrendoNaBlackFriday.collide(aviaodamulhermaravilha);
 
 //Desenha todos os sprites
 drawSprites();
+text(PONTOSSSS,500,50);
+PONTOSSSS+=Math.round(frameCount/60);
 
 //Chamar a função que vai criar as nuvens
 oporcovoltou();
+
+//Chamar a função que vai criar os cactos
+jorge();
 }
 
 function oporcovoltou(){
@@ -81,4 +102,30 @@ if(frameCount%60 === 0) {
     dinossauroCorrendoNaBlackFriday.depth +=1;
 }
 
+}
+
+function jorge(){
+   if(frameCount%60 === 0) {
+    var jorginhos = createSprite(600, 165, 10, 40);
+    jorginhos.velocityX = -6;
+    
+    var zeninguem = Math.round(random(1,6));
+
+    switch(zeninguem){
+        case 1: jorginhos.addImage(jorginho1);
+        break;
+        case 2: jorginhos.addImage(jorginho2);
+        break;
+        case 3: jorginhos.addImage(jorginho3);
+        break;
+        case 4: jorginhos.addImage(jorginho4);
+        break;
+        case 5: jorginhos.addImage(jorginho5);
+        break;
+        case 6: jorginhos.addImage(jorginho6);
+        break;
+        default: break;
+    }
+    jorginhos.scale = 0.5;
+   }
 }
